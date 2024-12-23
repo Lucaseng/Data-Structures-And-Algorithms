577b074393264fe27f4f0c7b831f84923ab8845c
lucaseng <lucasengnz@outlook.com>
2024-12-24 10:58:32 +1300

    feat: Create Node class and perform traversals on built trees

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import trees.Node;
+import trees.Traversals;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter values separated by spaces and use 'x' to specify an empty child:");
+        System.out.println("E.g. 5 4 3 x x 8 x x 6 x x");
+        Node<Integer> root = buildTree(splitWords(scanner.nextLine()).iterator(), Integer::parseInt);
+        //Node<String> root = buildTree(splitWords(scanner.nextLine()).iterator(), Function.identity());
+        scanner.close();
+        // You can now use the root node for traversal or other operations
+        var integerTraversals = new Traversals<Integer>();
+        //var stringTraversals = new Traversals<String>();
+        System.out.println("Preorder, inorder, and postorder traversals:");
+        integerTraversals.preorderTraversal(root);
+        System.out.println("");
+        integerTraversals.inorderTraversal(root);
+        System.out.println("");
+        integerTraversals.postorderTraversal(root);
+    }
+
+    public static <T> Node<T> buildTree(Iterator<String> iter, Function<String, T> f) {
+        String val = iter.next();
+        if (val.equals("x")) return null;
+        Node<T> left = buildTree(iter, f);
+        Node<T> right = buildTree(iter, f);
+        return new Node<T>(f.apply(val), left, right);
+    }
+
+    public static List<String> splitWords(String s) {
+        return s.isEmpty() ? List.of() : Arrays.asList(s.split(" "));
+    }
+}
---
+++ src/trees/Traversals.java
@@ -0,0 +1,30 @@
+package trees;
+
+public class Traversals<T> {
+    public void preorderTraversal(Node<T> root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.val + " ");
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+    }
+
+    public void inorderTraversal(Node<T> root) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left);
+        System.out.print(root.val + " ");
+        inorderTraversal(root.right);
+    }
+
+    public void postorderTraversal(Node<T> root) {
+        if (root == null) {
+            return;
+        }
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        System.out.print(root.val + " ");
+    }
+}
